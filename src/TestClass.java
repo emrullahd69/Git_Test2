492b66c66f3c3b3d8f82621534a0039dfb9fffe5
Emrullah <emrullahd69@gmail.com>
2020-08-08 21:32:50 +0300

    last commit from local master branch

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,8 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
         System.out.println("Our second commit from locl repository");
+        System.out.println("I want to have conflict");
+
+        System.out.println("I want to have a second conflict");
     }
 }
