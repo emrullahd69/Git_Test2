53a63dd4916a07c652094d97c373ae4547e07b38
Emrullah <emrullahd69@gmail.com>
2020-08-08 21:22:46 +0300

    Feature branch next commit

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
         System.out.println("Our second commit from local repository");
+        System.out.println("feature branch next commit");
     }
 }
