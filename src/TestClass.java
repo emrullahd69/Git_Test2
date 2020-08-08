dafd3bc8268036bad13a067035c37806a8075a11
emrullahd69 <69113305+emrullahd69@users.noreply.github.com>
2020-08-08 22:52:29 +0300

    Update TestClass.java
    I have committed.

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,7 +3,8 @@ public class TestClass {
         System.out.println("Git den merhaba!");
         System.out.println("Our second commit from locl repository");
         System.out.println("I want to have conflict");
-
+        
         System.out.println("I want to have a second conflict");
+        System.out.println("I dont't want a conflict");
     }
 }
